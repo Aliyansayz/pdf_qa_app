ebca71ed72a3b414ebb8cd6bdd90422dcb492dec
Aliyan Anwar <muhammad.aliyan22@gmail.com>
2023-12-30 17:30:12 +0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -57,24 +57,18 @@ function record() {
 }
 
 
-// suggestions.js
 
-// Wait for the document to be ready
 $(document).ready(function() {
-    // Select the text input with the id of "query"
     $("#message-input").autocomplete({
-        // Specify a function for the source option
         source: function(request, response) {
             // Use the jQuery ajax method to request the suggestions from the Flask app
             $.ajax({
                 url: "/suggestions",
                 data: {
-                    // Pass the term parameter with the value of the request
                     term: request.term
                 },
                 dataType: "json",
                 success: function(data) {
-                    // Pass the data to the response callback
                     response(data);
                 }
             });
